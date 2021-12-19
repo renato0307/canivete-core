01fb95b873605c53b06747ca1f17f21cea73c5a0
Renato Torres <renato.torres@outsystems.com>
2021-12-19 10:55:12 +0000

    feat: add datetime/fromunix

---
+++ pkg/datetime/fromunix_test.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2021 Renato Torres <renato.torres@pm.me>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package datetime
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromUnitTimestamp(t *testing.T) {
+	// act
+	p := Service{}
+	output := p.FromUnitTimestamp(1638964800)
+
+	// assert
+	assert.Equal(t, output.UtcTimestamp, "Wed Dec  8 12:00:00 UTC 2021")
+}
